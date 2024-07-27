e3dcd379f559f8dba090d2f45ad9f6b8bfe18533
khanhbqph <khanhbqph21610@fpt.edu.vn>
2024-07-27 19:18:53 +0700

    Resolved merge conflicts

---
+++ app/src/main/java/fpt/khanhbqph21610/asm/Gratitude.java
@@ -0,0 +1,32 @@
+package fpt.khanhbqph21610.asm;
+
+public class Gratitude {
+    private int id;
+    private String content;
+    private long date;
+    private int userId;
+
+    public Gratitude(int id, String content, long date, int userId) {
+        this.id = id;
+        this.content = content;
+        this.date = date;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
+
